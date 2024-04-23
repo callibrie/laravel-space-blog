91b84c86cefee2a9449c3a9b00851a97b69835f2
Callibrie <camgabriel.bit@gmail.com>
2024-04-23 12:25:00 +0900

    Create Post model, controller, view and policies

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('dashboard');
@@ -10,6 +11,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('posts', [PostController::class, 'index'])->name('posts');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
